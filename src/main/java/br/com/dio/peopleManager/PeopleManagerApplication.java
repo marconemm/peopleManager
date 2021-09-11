3640fc870495e3ba429cde66d9f4a7e8e48d7287
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-09-11 17:00:10 -0300

    Maven initial configuration.

---
+++ src/main/java/br/com/dio/peopleManager/PeopleManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.peopleManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/peopleManager/PeopleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.peopleManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
